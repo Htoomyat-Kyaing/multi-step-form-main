bbb32d8bd6f366756e727d506acc6bab7004ebe7
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2023-01-19 20:03:17 +0630

    header nav mobile

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        marine_blue: "hsl(var(--color-marine-blue) / <alpha-value>)",
+        purplish_blue: "hsl(var(--color-purplish-blue) / <alpha-value>)",
+        pastel_blue: "hsl(var(--color-pastel-blue) / <alpha-value>)",
+        light_blue: "hsl(var(--color-light-blue) / <alpha-value>)",
+        strawberry_red: "hsl(var(--color-strawberry-red) / <alpha-value>)",
+      },
+      gridTemplateRows: {
+        layout: "172px auto 100px",
+      },
+    },
+  },
+  plugins: [],
+};
