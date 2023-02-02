be7401fe2677cb9302ede1b2cae1e42a6b87c6b6
Htoomyat-Kyaing <94776906+Htoomyat-Kyaing@users.noreply.github.com>
2023-02-02 19:51:07 +0630

    solved input value

--- assets/js/addon.js
+++ assets/js/addon.js
@@ -1,5 +1,13 @@
-const serviceChecked = $("#service");
+let service = $("#service");
+let storage = $("#storage");
+let customize = $("#customize");
 
-serviceChecked.click(function () {
-  console.log("service");
+service.on("click", () => {
+  console.log(service.is(":checked"));
+});
+storage.on("click", () => {
+  console.log(storage.is(":checked"));
+});
+customize.on("click", () => {
+  console.log(customize.is(":checked"));
 });
